74682acebaae4a457e49f109629cf2257c22794a
Godwin Duliente <rualgodwin@gmail.com>
2023-01-01 11:48:44 +0800

    added responsive admin sidebar

--- custom.js
+++ custom.js
@@ -7,8 +7,21 @@ faqs.forEach(faq => {
     });
 });
 
-// Sidebar Module 
+// Search Sidebar Module 
 function show() {
     document.getElementById('sidebar').classList.toggle('active');
     document.getElementsByClassName('toggle-btn').classList.toggle('active');
 }
+
+// Admin Sidebar Module 
+let btn = document.querySelector("#btn");
+let sidebar = document.querySelector(".sidebar");
+let searchBtn = document.querySelector(".fa-magnifying-glass");
+
+btn.onclick = function() {
+    sidebar.classList.toggle('active');
+}
+
+searchBtn.onclick = function() {
+    sidebar.classList.toggle('active');
+}        
